967758797ac5af0a3f2b598b3bffac8b716024f0
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-19 04:43:27 +0100

    added new elements to index

---
+++ index.js
@@ -0,0 +1,15 @@
+const greeting = prompt("What is your name?")
+
+console.log(greeting)
+
+const age = prompt("What is your age?")
+
+const actualAge = alert(new Date().getFullYear() - 1981)
+
+console.log(actualAge)
+
+const number = parseInt(someStringThatLooksLikeANumber)
+
+const numbers = alert('Give me 2 numbers')
+
+console.log(numbers)
