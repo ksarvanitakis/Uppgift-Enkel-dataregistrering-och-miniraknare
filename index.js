8fb1644435e182b54827cd87bdbbd9b12e090dd2
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-19 16:57:02 +0100

    assigned new values to variables

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ console.log(greeting)
 
 const age = prompt("What is your age?")
 
-const actualAge = alert(new Date().getFullYear() - 1981)
+console.log(age);
+
+const actualAge = alert(new Date().getFullYear(2024) - new Date().getFullYear(1981))
 
 console.log(actualAge)
 
-const number = parseInt(someStringThatLooksLikeANumber)
+const number = parseInt("45")
 
 const numbers = alert('Give me 2 numbers')
 
-console.log(numbers)
+console.log(number)
+
+confirm("Multiply the " + numbers)
+
