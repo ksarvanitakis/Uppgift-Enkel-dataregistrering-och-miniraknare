a5d5d219e55e3d2c70adfd4d22e3658c89969e1e
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-22 16:21:30 +0100

    added a counter

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ console.log(greeting + " your age is " + age + " years old!")
 
 const number = parseInt("your age is " + age + " years old!")
 
-const message = alert (greeting + " all the counts are now done. Go and check them in the console!")
+const message = alert(greeting + " all the counts are now done. Go and check them in the console!")
+
+
+
+//Miniraknare//
+
+const fistQuestion = prompt("Pick a language")
+
+const secondQuestion = prompt("Pick another language")
+
+const trirdQuestion = prompt("Which counting system would you like to choose?")
