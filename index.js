dffc88a390e041b2ce1ac2490ae1fbcab3480bc7
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-22 10:29:12 +0100

    all done

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const year = prompt("What is your year of birth")
 const age = new Date().getFullYear(2024) - year
 
 console.log(greeting + " your age is " + age + " years old!")
+
+const number = parseInt("your age is " + age + " years old!")
+
+const message = alert (greeting + " all the counts are now done. Go and check them in the console!")
