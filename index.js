a4057611eb23841a334478b85360ee0dcf64f1fd
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-01-22 10:21:54 +0100

    added a user greeting in the console

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const greeting = prompt("What is your name?")
 
-console.log(greeting)
+console.log("Hallo " + greeting)
 
-const age = prompt("What is your age?")
+const year = prompt("What is your year of birth")
 
-console.log(age);
+const age = new Date().getFullYear(2024) - year
 
-const actualAge = alert(new Date().getFullYear(2024) - new Date().getFullYear(1981))
-
-console.log(actualAge)
-
-const number = parseInt("45")
-
-const numbers = alert('Give me 2 numbers')
-
-console.log(number)
-
-confirm("Multiply the " + numbers)
-
+console.log(greeting + " your age is " + age + " years old!")
